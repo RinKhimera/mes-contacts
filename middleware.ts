37013915d5b1b3176562c77ed2c2fd0ead2a7ee2
Samuel Pokam <dixiades@gmail.com>
2025-01-12 11:36:47 -0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 
-const isProtectedRoute = createRouteMatcher(["/post(.*)", "/my-profile(.*)"])
+const isProtectedRoute = createRouteMatcher([
+  "/post",
+  "/my-profile(.*)",
+  // "/contact-us(.*)",
+])
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect()
